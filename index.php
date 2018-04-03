ecbf3c2293b8e10f5c78c6143d1cda7d7e7a671a
Marco Van der Eecken <marco@valkyriweb.com>
2018-04-03 15:56:14 +0200

    Quote generation

--- index.php
+++ index.php
@@ -8,64 +8,11 @@ echo "<pre>";
 print_r($thisQuote);
 echo "</pre>";
 
-git add README.md
- echo "Yeah";
+$response = $myQuote->createRequest();
 
-
-
-
-
-
-
-
-
-
-/* 
-file_put_contents('log.txt', "Start");
-/* $fp = fopen('log2.txt', '23123123123'); */
-/* file_put_contents('log2.json', file_get_contents('php://input')); */
-/* file_put_contents('log2.txt', $_POST); */
-
-/* 
- if($json = json_decode($_POST, true)) {
-
-     print_r($json);
-
-     $data = $json;
-
- } else {
-
-     print_r($_POST);
-
-     $data = $_POST;
-
- }
-
- file_put_contents('log.txt', "After JSON connect");
-
- echo "Saving data ...\n";
-
- $url = "http://localhost:8081";
-
- $meta = ["received" => time(),
-
-     "status" => "new",
-
-     "agent" => $_SERVER['HTTP_USER_AGENT']];
-
- $options = ["http" => [
-
-     "method" => "POST",
-
-     "header" => ["Content-Type: application/json"],
-
-     "content" => json_encode(["data" => $data, "meta" => $meta])]
-
-     ];
-file_put_contents('log.txt', "creating post");
-
- $context = stream_context_create($options);
-
- $response = file_get_contents($url, false, $context);
-
- file_put_contents('log.txt', $response); */
+try {
+    $response = $request->send();
+    echo $response->getBody();
+} catch (HttpException $ex) {
+    echo $ex;
+}
--- model/Quote.php
+++ model/Quote.php
@@ -6,7 +6,13 @@ class Authorization {
 
 class ClientChat {
     public $clientID, $clientFirstName, $clientLastName;
-
+    
+    /** Hi I am a client chat
+     * ClientChat constructor.
+     * @param $clientID
+     * @param $clientFirstName
+     * @param $clientLastName
+     */
     public function __construct($clientID, $clientFirstName, $clientLastName) {
         $this->clientID($clientID);
         $this->clientFirstName($clientFirstName);
@@ -29,5 +35,24 @@ class Quote extends Authorization {
             "type" => $this->type,
             "model_name" => $this->modelName
         ];
+    }
+    
+    public function createRequest() {
+        $request = new HttpRequest();
+        $request->setUrl('https://sandbox.root.co.za/v1/insurance/quotes');
+        $request->setMethod(HTTP_METH_POST);
+    
+        $request->setHeaders(array(
+//            'Postman-Token' => '396500b4-20be-40bc-94f8-f35ed15aef89',
+//            'Cache-Control' => 'no-cache',
+            'Authorization' => 'Basic ' . base64_encode(Authorization::$sandbox),
+            'Content-Type' => 'application/json'
+        ));
+    
+        $request->setBody('{
+            "type": "root_gadgets",
+            "model_name": "iPhone 5"
+        }');
+        return $request;
     }
 }
